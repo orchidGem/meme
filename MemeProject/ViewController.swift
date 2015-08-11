8a4133767f56707dc6dcca8be5f7bdc7d25c83dd
Laura Evans <laura.evans@ivieinc.com>
2015-08-11 12:27:29 -0500

    Initial Commit

---
+++ MemeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeProject
+//
+//  Created by Laura Evans on 8/11/15.
+//  Copyright (c) 2015 Ivie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
